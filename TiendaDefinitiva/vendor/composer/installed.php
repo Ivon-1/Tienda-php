a4f529d8052b2a57043dc0f7961465bf6145c6eb
Ivon-1 <IvanAlvz12@outlook.es>
2025-04-13 15:16:20 +0200

    Actualizacion 3

---
+++ TiendaDefinitiva/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ivana/tienda-definitiva',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '445e4fdc73422a854bdc9d1dd0978da6765142c0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ivana/tienda-definitiva' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '445e4fdc73422a854bdc9d1dd0978da6765142c0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
